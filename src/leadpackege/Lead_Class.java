b4919cc62ae2da672e8e6b533b7e76a2343644fb
shashi <shashi@LAPTOP-BKEQF5PU>
2020-06-15 17:43:01 +0530

    lead project commit..

---
+++ src/leadpackege/Lead_Class.java
@@ -0,0 +1,19 @@
+package leadpackege;
+
+public class Lead_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String task_assigned ="yes";
+		
+		if (task_assigned=="yes"||task_assigned=="YES"||task_assigned=="y")
+			System.out.println("code something");
+				
+		else {
+			System.out.println("update lead-NO work Assigned");
+			
+		}
+
+	}
+
+}
